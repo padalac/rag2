7ff7e1188052fe9ac51a46d89d3cbd7dc0a48f41
padalac <121174907+padalac@users.noreply.github.com>
2023-12-15 22:49:53 +0530

    possible race between chroma persist and exit

--- main2.py
+++ main2.py
@@ -93,5 +93,6 @@ if __name__ == "__main__":
                         return_source_documents=True
                         )
         qa_retriever.vectorstore.persist()
+        time.sleep(5)
         if rag_config['DEFAULT']['mode'] == 'update_only' :
             exit(0)
